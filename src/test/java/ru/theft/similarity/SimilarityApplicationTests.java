7b6ca3a4653595e4ea0f9f5c353a15d6841df6b9
m3Rzy <gentledemeiz@gmail.com>
2024-09-19 19:14:40 +0300

    1st beta version

---
+++ src/test/java/ru/theft/similarity/SimilarityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.theft.similarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
